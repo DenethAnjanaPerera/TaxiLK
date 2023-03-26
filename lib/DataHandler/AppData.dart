cba0629eb8dd8746dbea6616848c212fd19f8482
Deneth Perera <denethanjana95@gmail.com>
2023-03-26 06:29:32 +0530

    Print Auto Complete Listner

--- lib/DataHandler/AppData.dart
+++ lib/DataHandler/AppData.dart
@@ -4,8 +4,8 @@ import 'package:taxilk/Models/address.dart';
 class AppData extends ChangeNotifier{
 
   Address pickUplocation;
-  void updatePickuplocationAddress(Address pickUpAddress)
-  pickUplocation = pickUpAddress;
-  notifyListeners();
-
+  void updatePickuplocationAddress(Address pickUpAddress) {
+    pickUplocation = pickUpAddress;
+    notifyListeners();
+  }
 }
