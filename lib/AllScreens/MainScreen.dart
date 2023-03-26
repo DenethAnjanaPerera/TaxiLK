3a3fff3b9a75590b3b67d162196ecc883a133268
Deneth Perera <denethanjana95@gmail.com>
2023-03-26 22:43:23 +0530

    Requesting Rider Message Display

--- lib/AllScreens/MainScreen.dart
+++ lib/AllScreens/MainScreen.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
@@ -42,8 +43,17 @@ class _State extends State<MainScreen> with TickerProviderStateMixin{
   Set<Marker>markersSet={};
   Set<Circle>circleSet={};
   double rideDetailsContainerHeight=0;
+  double requestRideContainerHeight=0;
   double searchContainerHeight=300.0;
   bool drawerOpen=true;
+  void displayRequestRideContainer(){
+    setState(() {
+      requestRideContainerHeight=250.0;
+      rideDetailsContainerHeight=0;
+      bottomPaddingOfMap=230.0;
+      drawerOpen=true;
+    });
+  }
 
   void displayRideDetailsContainer()async{
     await getPlaceDirection();
@@ -373,7 +383,7 @@ class _State extends State<MainScreen> with TickerProviderStateMixin{
                     Padding(padding:EdgeInsets.symmetric(horizontal: 16.0),child:
                     RaisedButton(
                       onPressed: (){
-                        print("Clicked");
+                        displayRequestRideContainer();
                       },
                       color: Theme.of(context).accentColor,
                       child: Padding(
@@ -399,6 +409,72 @@ class _State extends State<MainScreen> with TickerProviderStateMixin{
                 ),
               ),
               ),
+            ),
+          ),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(16.0),topRight:
+                  Radius.circular(16.0),
+
+                  ),color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(spreadRadius: 0.5,
+                      blurRadius: 16.0,
+                      color: Colors.black54,
+                      offset: Offset(0.7,0.7),),
+                  ]
+              ),height: requestRideContainerHeight,
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 12.0,),
+                    SizedBox(width: double.infinity,child: ColorizeAnimatedTextKit(
+                      onTap: (){
+                        print("Tap Event");
+                      },text: [
+                      "Requesting a Ride.... ",
+                      "Please Wait...",
+                      "Finding a Driver",
+                    ],textStyle: TextStyle(
+                      fontSize: 55.0,
+                      fontFamily: "Signatra",
+
+
+                    ),colors: [
+                      Colors.green,
+                      Colors.purple,
+                      Colors.pink,
+                      Colors.blue,
+                      Colors.yellow,
+                      Colors.red,
+                    ],textAlign: TextAlign.center,
+
+
+                    ),
+                    ),
+                    SizedBox(height: 22.0,),
+                    Container(
+                      height: 60.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(26.0),
+                        border: Border.all(width: 2.0,color: Colors.grey[300]),
+
+                      ),child: Icon(Icons.close,size: 26.0,),
+
+                    ),SizedBox(height: 10.0,),
+                    Container(width: double.infinity,
+                      child: Text("Cancel Ride",textAlign: TextAlign.center,style:
+                      TextStyle(fontSize: 12.0),),),
+                  ],
+                ),
+              ),alignment: AlignmentDirectional.topStart,
+
             ),
           ),
         ],
